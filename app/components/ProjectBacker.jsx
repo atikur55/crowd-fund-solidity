93e4f2647c1f78481c8279220525da70c3e215f1
atikur55 <atikcseuu40@gmail.com>
2024-11-12 16:01:20 +0600

    v1.2

--- app/components/ProjectBacker.jsx
+++ app/components/ProjectBacker.jsx
@@ -2,9 +2,9 @@ import { Currency, EarthLock } from "lucide-react";
 
 const ProjectBacker = () => {
   return (
-    <div className="py-24 px-6 flex flex-col justify-center items-center">
+    <div className="py-24 px-6 flex flex-col justify-center items-start md:w-2/3 mx-auto">
       <div className="max-h-[calc(100vh_-_20rem)] overflow-y-auto shadow-md rounded-md w-full">
-        <table className="min-w-full">
+        <table className="min-w-full mb-5">
           <thead className="border-b">
             <tr>
               <th
@@ -34,28 +34,32 @@ const ProjectBacker = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="border-b border-gray-200">
-              <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
-                <div className="flex items-center space-x-1">
-                  <EarthLock size={16} />
-                  <small className="text-gray-700 font-bold">
-                    0x9e.....13af
-                  </small>
-                </div>
-              </td>
-              <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
-                <small className="flex justify-start items-center space-x-1">
-                  <Currency size={16} />
-                  <span className="text-gray-700 font-bold">{3} ETH</span>
-                </small>
-              </td>
-              <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
-                Data
-              </td>
-              <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
-                Data
-              </td>
-            </tr>
+            {Array(5)
+              .fill()
+              .map((backing, i) => (
+                <tr key={i} className="border-b border-gray-200">
+                  <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
+                    <div className="flex items-center space-x-1">
+                      <EarthLock size={16} />
+                      <small className="text-gray-700 font-bold">
+                        0x9e.....13a{i}
+                      </small>
+                    </div>
+                  </td>
+                  <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
+                    <small className="flex justify-start items-center space-x-1">
+                      <Currency size={16} />
+                      <span className="text-gray-700 font-bold">{3} ETH</span>
+                    </small>
+                  </td>
+                  <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
+                    {false ? "Yes" : "No"}
+                  </td>
+                  <td className="text-sm font-light px-6 py-4 whitespace-nowrap">
+                    {new Date().getTime()}
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
